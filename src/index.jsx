ee18f59ccd2c72a07d99741e539f878b064d4aca
Yevgen Sukharenko <ysukharenko@gmail.com>
2019-08-07 00:33:04 +0300

    Comments

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+
+// https://phrase.com/blog/posts/react-i18n-best-libraries/
+// npm install i18next react-i18next i18next-browser-languagedetector --save
 import { I18nextProvider } from 'react-i18next';
 
 import i18n from './i18n';
